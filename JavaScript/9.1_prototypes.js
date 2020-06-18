fa671dbd0bb61b4ab51ac84495f91b5937b6b4d4
Deepansh Srivastava <deepharshsri.98@gmail.com>
2020-06-18 13:39:40 +0530

    Prototype done

--- JavaScript/9.1_prototypes.js
+++ JavaScript/9.1_prototypes.js
@@ -14,5 +14,11 @@ console.log(r)
 console.log(r.c)
 console.log(r.a)
 console.log(r.b)
+console.log(r.__proto__.__proto__==p)
 console.log(r.__proto__==q)
+console.log(q.c)
+console.log(r.a)
+
+let a="ngksengks"
+console.log(a.__proto__.__proto__) 
 
--- JavaScript/newKeyword.js
+++ JavaScript/newKeyword.js
@@ -1,24 +1,35 @@
 // console.log(this==global)// false
 
-function foo(){
+function foo(a){
     
     // console.log(this==global)// true bcz we are calling function and by this time "this" will refer to Global object .
    
-       this.a="something"
-       b=13;
+       this.a=a
+         let b=13 
        console.log(this==global)
        return 10;
 
 
 }
-// let o1=foo()
+let o1=foo("harry potter")
+console.log(a)
+console.log(b)
 
-let o2= new foo();
+// let o2= new foo();
 
-// console.log(o1)
-// console.log(o1.a)
-// console.log(o1.b)
-console.log(o2)
-console.log(o2.a)
-console.log(o2.b)
+
+// // console.log(o1)
+// // console.log(o1.a)
+console.log(o1.b)
+// console.log(o2)
+// console.log(o2.a)
+// console.log(o2.b)
+let oo={
+    a:10,b:"feepe"}
+    console.log(oo.b)
+
+    f=10
+ console.log(typeof f)
+
+
 
